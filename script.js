e245ff083ef2ea420e49ddcfed35a8903ef8e244
Vova <fridrix.09@gmail.com>
2022-10-20 01:13:17 +0300

    Update script.js
    second

--- script.js
+++ script.js
@@ -24,3 +24,11 @@
 'use strict';
 
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
