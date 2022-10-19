d32ab1154fbe70c636247ebfdbe7c83772f04b84
Vova <fridrix.09@gmail.com>
2022-10-20 01:23:36 +0300

    firstDone

--- script.js
+++ script.js
@@ -32,3 +32,16 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+
+const a = prompt('Один из последних просмотренных фильмов?'),
+      b = prompt('На сколько оцените его?'),
+      c = prompt('Один из последних просмотренных фильмов?'),
+      d = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
